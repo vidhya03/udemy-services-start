c79952216bf0fe18b62600844352e20ec8834ec0
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-07-13 23:08:00 +0530

    services added

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,22 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LogggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LogggingService]
+  providers: [LogggingService, AccountsService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
-  constructor(private loggingService: LogggingService) {}
+
+  constructor(
+    private loggingService: LogggingService,
+    private accountsService: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
     this.loggingService.logStatusChange(accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
   }
 }
