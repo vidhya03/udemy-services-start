e26a8667158592d9d2b3d00658363c20b0a15a7d
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-07-14 23:32:42 +0530

    upto accounts update"

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,20 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { LogggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LogggingService]
+  providers: [LogggingService, AccountsService]
 })
-export class AccountComponent {
-  @Input() account: {name: string, status: string};
+export class AccountComponent implements OnInit {
+  @Input() account: { name: string; status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LogggingService) {}
+
+
+  constructor(private loggingService: LogggingService,
+              private accountsService: AccountsService) {}
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateStatus( this.id, status );
     this.loggingService.logStatusChange(status);
   }
+
+  ngOnInit() {}
 }
