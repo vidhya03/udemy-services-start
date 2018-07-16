877d3c9c31583f609ae12758b45ddf5f4b10b7ed
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-07-16 22:06:44 +0530

    added logging service to lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
-import { AccountsService } from './accounts.service';
-import { LogggingService } from './logging.service';
+// import { AccountsService } from './accounts.service';
+// import { LogggingService } from './logging.service';
 
 @NgModule({
   declarations: [AppComponent, AccountComponent, NewAccountComponent],
   imports: [BrowserModule, FormsModule, HttpModule],
-  providers: [ LogggingService],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
