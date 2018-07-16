d4d1cbaebd800c2d523b87dd083041186666babd
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-07-16 22:05:30 +0530

    added logging service to lazy loading

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
 export class LogggingService {
   logStatusChange(status: string) {
     console.log('A server status changed, new status: ' + status);
